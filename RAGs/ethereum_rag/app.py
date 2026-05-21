eeb61c32fe5329af79599986bf6405a5ce255a41
ianwright27 <thewian27@gmail.com>
2026-05-21 20:04:03 +0300

    RAG setup

---
+++ RAGs/ethereum_rag/app.py
@@ -0,0 +1,53 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_ollama import OllamaEmbeddings
+from langchain_ollama import ChatOllama
+
+
+# load the PDF document
+loader = PyPDFLoader("docs/introduction_to_ethereum.pdf")
+docs = loader.load()
+
+# split text into chunks
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
+splits = text_splitter.split_documents(docs)
+
+# create embeddings
+embeddings = OllamaEmbeddings(model="qwen2.5:3b")
+
+# Store in Chroma vector database 
+vectorstore = Chroma.from_documents(
+    documents=splits,
+    embedding=embeddings,
+    persist_directory="./chroma_db"
+)
+
+# user query 
+query = "What is Ethereum?"
+
+# retrieve rlevant chunks
+retriever = vectorstore.as_retriever()
+relevant_docs = retriever.invoke(query)
+
+# Local LLM
+llm = ChatOllama(model="qwen2.5:3b")
+
+# Build prompt 
+context = "\n\n".join([doc.page_content for doc in relevant_docs]) 
+
+prompt = f"""
+Answer the question using the context below. 
+
+Context:
+{context}
+
+Question:
+{query}
+"""
+
+# Generate a response
+response = llm.invoke(prompt)
+
+print("\nANSWER:\n")
+print(response.content)
